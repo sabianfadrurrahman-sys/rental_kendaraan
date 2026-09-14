35012d87d8e28208da54a29ad07ac7640312c2cf
sabianfadrurrahman <sabianfadrurrahman@gmail.com>
2026-09-14 15:01:39 +0700

    [ADD] nti_vehicle_rental: buat kerangka dasar model rental.order

---
+++ nti_vehicle_rental/models/rental_order.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+class RentalOrder(models.Model):
+    _name = 'rental.order'
+    _description = 'Rental Order'
+    _order = 'name desc'
+    _rec_name = 'name'
+
+    name = fields.Char(string='Order Reference', required=True, copy=False, readonly=True, default=lambda self: 'New')
+    
